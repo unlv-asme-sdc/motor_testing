e7e313b43ed1ac5d6ee2a8c0da6594330b7e39bc
Patrick Messimer <patrickmessimer@gmail.com>
2017-11-25 14:48:26 -0800

    created 1 flywheel test program

--- 2_flywheel_test/_1_flywheel_test/_1_flywheel_test.ino
+++ 2_flywheel_test/_1_flywheel_test/_1_flywheel_test.ino
@@ -1,19 +1,19 @@
+int error;
+byte vibrate = 0;
+
 int enable1 = 11;
 int pow1 = 12;
 int dir1 = 13;
-
 int cs1 = 0;
 
 int enable2 = 8;
 int pow2 = 9;
 int dir2 = 10;
-
 int cs2 = 1;
 
 
 void setup() {
   Serial.begin(250000);
-
   //setup driver pins
   pinMode(enable1, OUTPUT);
   pinMode(pow1, OUTPUT);
@@ -22,23 +22,18 @@ void setup() {
   pinMode(enable2, OUTPUT);
   pinMode(pow2, OUTPUT);
   pinMode(dir2, OUTPUT);
-
- 
 }
 
 void loop() {
- 
+
   //enable drivers
-  digitalWrite(enable1, HIGH);
   digitalWrite(enable2, HIGH);
+  digitalWrite(enable1, LOW);
 
-  digitalWrite(dir1, LOW);
-  analogWrite(pow1, 255);
-
+  //set each motor 2's direction and speed
   digitalWrite(dir2, LOW);
   analogWrite(pow2, 255);
-
-  Serial.print(analogRead(cs1));
+  Serial.print(analogRead(cs2));
   Serial.print("\t");
-  Serial.println(analogRead(cs2));
+
 }
